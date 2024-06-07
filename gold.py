87a79e5d18e7acb2cca5d558a14831b319de52ae
evecoq <49597501+evecoq@users.noreply.github.com>
2024-06-07 18:09:05 +0200

    Update gold.py

--- gold.py
+++ gold.py
@@ -1,8 +1,18 @@
-#--------------------------------GOLD LEVEL-----------------------------
+
+# Create Spark session
+spark = SparkSession.builder \
+    .appName("Silver_to_Gold") \
+    .getOrCreate()
 
 # Load Titles dataset from HDFS /silver/
-# Load Ratings dataset from HDFS /silver/
+csv_t = "hdfs://namenode/user/output/imdbdata/silver/titles_df.csv"
+titles_df = spark.read.csv(csv_t, header=True, inferSchema=True)
+titles_df.show(5)
 
+# Load Ratings dataset from HDFS /silver/
+csv_r = "hdfs://namenode/user/output/imdbdata/silver/ratings_df.csv"
+ratings_df = spark.read.csv(csv_r, header=True, inferSchema=True)
+ratings_df.show(5)
 
 # Merge both tables (we want to keep all the rows from ratings_df table because it's has rating by each episode of a TV-show and not only by title)
 df = titles_df.join(ratings_df, on="tconst", how="right")
@@ -24,6 +34,12 @@ plt.xlabel('Average Rating')
 plt.ylabel('Frequency')
 plt.show()
 
+# Save the plot locally
+local_plot1 = "/tmp/Average Ratings histogram.png"
+plt.savefig(local_plot1)
+plt.close()
+
+
 # Genre count barplot
 genre_counts = titles_df.groupBy('genre_1').count()
 genre_counts = genre_counts.orderBy('count', ascending=False)
@@ -37,6 +53,12 @@ plt.xlabel('Number of Titles')
 plt.ylabel('Genre')
 plt.show()
 
+# Save the plot locally
+local_plot2 = "/tmp/Genre count barplot.png"
+plt.savefig(local_plot2)
+plt.close()
+
+
 # Correlation Heatmap
 pandas_df = df.select('averageRating', 'numVotes', 'startYear', 'endYear', 'runtimeMinutes', 'isAdult').toPandas()
 plt.figure(figsize=(10, 8))
@@ -45,5 +67,11 @@ sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
 plt.title('Correlation Heatmap')
 plt.show()
 
+# Save the plot locally
+local_plot3 = "/tmp/Correlation Heatmap.png"
+plt.savefig(local_plot3)
+plt.close()
+
 
 # Export df into HDFS /gold/
+df.write.csv("hdfs://your_hdfs_cluster/path/to/imdbdata/gold", header=True, mode="overwrite")
